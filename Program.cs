3acea65018bbc2655ce8d0a3c9b146ceaeea28b4
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-20 14:46:44 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RefactoringCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Refactoring --modifing the structure of the code, but keeping the behaviour !
+            // refactoring is a natural process of fixing  the bugs/ its is recommended when we test the code to refactor it.
+            // code review, codeReviewing --to check the written code  according to conventions!-If we have a new collegue, My Boss makes it.
+            // we refactor when We make test -driven development, 
+            //We do refactoring when we have a technical debt !, we know about the bugs but we will fix it later.
+            //Principles in Refacturing - > Keep it Simple Stupid, KISS principle !
+            // BoyscoutRule--Leave the code better than You found it !
+            // avoid duplicate Code !, 
+            // the code to be good written--ie good-written code is self-documenting, self-explaining 
+            // appropriate level of Abstraction !
+            // keep the refactoring on small pieces, then test, then refactor again .!
+            // we change a method-test, then commit in GitHub, step by step, checkBox--all problematic things.
+            // Make a CheckList !!!
+            // the Code smells--then we need to refactor !
+            // --Long method , long class , Long paramethers list, 
+            // Code Smells -- A, B, C - > impropper Names ! 
+            // the Scope of {} -> must be well formated !
+            // inconsistency of code - > the name of the method is not propper of what he does .
+            //
+             // Object Oriented abusement 
+             // Wrong --a class depend ot its inheritant ! 
+             // Base class can not depends on its descendant !
+             // avoid using static, class and methods .
+             // static with a serious logic is not ok !
+             // logger -on each call of a Method, creates logger history, who , when does athing !
+             // Duplicate code is a code smells !
+             // Dead code- old code and almost dont used, 
+             // Lazy class -class that does not do much and may be delited !
+             // interfaces are easily replaceable, the classes are not easily Interchangeable
+             // Methods LevelRefactorung, 
+             // Class Level refactoring !
+
+
+        }
+    }
+}
